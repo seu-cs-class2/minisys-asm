ad37a162dc11e52125dc63e1da761cda1ff11262
z0gSh1u <zx.cs@qq.com>
2021-01-13 12:29:42 +0800

    fix: 调整node端异常处理

--- dist/entry/node.js
+++ dist/entry/node.js
@@ -20,59 +20,69 @@ var jOffset = 1280;
 function stdoutPrint(content) {
     process.stdout.write(content);
 }
-if (args._.length === 0 || args._.length !== 2) {
-    stdoutPrint('Usage: node <some_path>/node.js <in_file> <out_dir> [-l]\n');
-}
-else {
-    var inFile = args._[0];
-    var outDir = args._[1];
-    var link = !!args.l;
-    // @ts-ignore
-    globalThis._minisys = {
-        _userAppOffset: link ? jOffset : 0,
-    };
-    if (!link) {
-        var asmCode = fs_1.default.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim();
-        var asmResult = assembler_1.assemble(asmCode);
-        var dataCoe = convert_1.dataSegToCoe(asmResult.dataSeg);
-        var textCoe = convert_1.textSegToCoe(asmResult.textSeg);
-        fs_1.default.writeFileSync(path_1.default.join(outDir, 'dmem32.coe'), dataCoe);
-        fs_1.default.writeFileSync(path_1.default.join(outDir, 'prgmip32.coe'), textCoe);
-        fs_1.default.writeFileSync(path_1.default.join(outDir, 'serial.txt'), convert_1.coeToTxt(textCoe, dataCoe));
-        stdoutPrint('[minisys-asm] Assembling done.');
+try {
+    if (args._.length === 0 || args._.length !== 2) {
+        stdoutPrint('Usage: node <some_path>/node.js <in_file> <out_dir> [-l]\n');
     }
     else {
-        // 进行链接
-        var biosContent = fs_1.default.readFileSync(path_1.default.join(__dirname, '../snippet/minisys-bios.asm')).toString();
-        var userAppASM = fs_1.default.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim();
-        var intEntryContent = fs_1.default.readFileSync(path_1.default.join(__dirname, '../snippet/minisys-interrupt-entry.asm')).toString();
-        var intHandlerContent = fs_1.default
-            .readFileSync(path_1.default.join(__dirname, '../snippet/minisys-interrupt-handler.asm'))
-            .toString();
-        // 把链接后的代码段进行汇编
-        var asm = (userAppASM + '\n')
-            .replace(/\r\n/g, '\n')
-            .replace(/#(.*)\n/g, '\n')
-            .split('\n');
-        // 挑出代码段和数据段
-        var dataSegStartLine = asm.findIndex(function (v) { return v.match(/\.data/); });
-        var textSegStartLine = asm.findIndex(function (v) { return v.match(/\.text/); });
-        utils_1.assert(dataSegStartLine !== -1, '未找到数据段开始');
-        utils_1.assert(textSegStartLine !== -1, '未找到代码段开始');
-        utils_1.assert(dataSegStartLine < textSegStartLine, '数据段不能位于代码段之后');
-        // 链接完成后汇编
-        var allProgram = asm.slice(dataSegStartLine, textSegStartLine).join('\n') +
-            '\n' +
-            '.text\n' +
-            linker_1.linkAll(biosContent, asm.slice(textSegStartLine + 1).join('\n'), intEntryContent, intHandlerContent);
-        var all = assembler_1.assemble(allProgram);
-        var textCoe = convert_1.textSegToCoe(all.textSeg);
-        var dataCoe = convert_1.dataSegToCoe(all.dataSeg);
-        // 输出
-        fs_1.default.writeFileSync(path_1.default.join(outDir, 'prgmip32.coe'), textCoe);
-        fs_1.default.writeFileSync(path_1.default.join(outDir, 'dmem32.coe'), dataCoe);
-        fs_1.default.writeFileSync(path_1.default.join(outDir, 'serial.txt'), convert_1.coeToTxt(textCoe, dataCoe));
-        fs_1.default.writeFileSync(path_1.default.join(outDir, 'linked.asm'), allProgram);
-        stdoutPrint("[minisys-asm] Assembling done with linking. jOffset = " + jOffset + " B.");
+        var inFile = args._[0];
+        var outDir = args._[1];
+        var link = !!args.l;
+        // @ts-ignore
+        globalThis._minisys = {
+            _userAppOffset: link ? jOffset : 0,
+        };
+        if (!link) {
+            var asmCode = fs_1.default.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim();
+            var asmResult = assembler_1.assemble(asmCode);
+            var dataCoe = convert_1.dataSegToCoe(asmResult.dataSeg);
+            var textCoe = convert_1.textSegToCoe(asmResult.textSeg);
+            fs_1.default.writeFileSync(path_1.default.join(outDir, 'dmem32.coe'), dataCoe);
+            fs_1.default.writeFileSync(path_1.default.join(outDir, 'prgmip32.coe'), textCoe);
+            fs_1.default.writeFileSync(path_1.default.join(outDir, 'serial.txt'), convert_1.coeToTxt(textCoe, dataCoe));
+            stdoutPrint('[minisys-asm] Assembling done.');
+        }
+        else {
+            // 进行链接
+            var biosContent = fs_1.default.readFileSync(path_1.default.join(__dirname, '../snippet/minisys-bios.asm')).toString();
+            var userAppASM = fs_1.default.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim();
+            var intEntryContent = fs_1.default.readFileSync(path_1.default.join(__dirname, '../snippet/minisys-interrupt-entry.asm')).toString();
+            var intHandlerContent = fs_1.default
+                .readFileSync(path_1.default.join(__dirname, '../snippet/minisys-interrupt-handler.asm'))
+                .toString();
+            // 把链接后的代码段进行汇编
+            var asm = (userAppASM + '\n')
+                .replace(/\r\n/g, '\n')
+                .replace(/#(.*)\n/g, '\n')
+                .split('\n');
+            // 挑出代码段和数据段
+            var dataSegStartLine = asm.findIndex(function (v) { return v.match(/\.data/); });
+            var textSegStartLine = asm.findIndex(function (v) { return v.match(/\.text/); });
+            utils_1.assert(dataSegStartLine !== -1, '未找到数据段开始');
+            utils_1.assert(textSegStartLine !== -1, '未找到代码段开始');
+            utils_1.assert(dataSegStartLine < textSegStartLine, '数据段不能位于代码段之后');
+            // 链接完成后汇编
+            var allProgram = asm.slice(dataSegStartLine, textSegStartLine).join('\n') +
+                '\n' +
+                '.text\n' +
+                linker_1.linkAll(biosContent, asm.slice(textSegStartLine + 1).join('\n'), intEntryContent, intHandlerContent);
+            var all = assembler_1.assemble(allProgram);
+            var textCoe = convert_1.textSegToCoe(all.textSeg);
+            var dataCoe = convert_1.dataSegToCoe(all.dataSeg);
+            // 输出
+            fs_1.default.writeFileSync(path_1.default.join(outDir, 'prgmip32.coe'), textCoe);
+            fs_1.default.writeFileSync(path_1.default.join(outDir, 'dmem32.coe'), dataCoe);
+            fs_1.default.writeFileSync(path_1.default.join(outDir, 'serial.txt'), convert_1.coeToTxt(textCoe, dataCoe));
+            fs_1.default.writeFileSync(path_1.default.join(outDir, 'linked.asm'), allProgram);
+            stdoutPrint("[minisys-asm] Assembling done with linking. jOffset = " + jOffset + " B.");
+        }
+    }
+}
+catch (ex) {
+    if (ex instanceof utils_1.SeuError) {
+        stdoutPrint("[SeuError] " + ex.message + "\n");
+    }
+    else {
+        throw ex;
     }
 }
--- package/entry/node.ts
+++ package/entry/node.ts
@@ -8,7 +8,7 @@ import path from 'path'
 import { assemble } from '../assembler'
 import { coeToTxt, dataSegToCoe, textSegToCoe } from '../convert'
 import { linkAll } from '../linker'
-import { assert } from '../utils'
+import { assert, SeuError } from '../utils'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const args = require('minimist')(process.argv.slice(2))
@@ -19,58 +19,66 @@ function stdoutPrint(content: string) {
   process.stdout.write(content)
 }
 
-if (args._.length === 0 || args._.length !== 2) {
-  stdoutPrint('Usage: node <some_path>/node.js <in_file> <out_dir> [-l]\n')
-} else {
-  const inFile = args._[0]
-  const outDir = args._[1]
-  const link = !!args.l
-  // @ts-ignore
-  globalThis._minisys = {
-    _userAppOffset: link ? jOffset : 0,
-  }
-  if (!link) {
-    const asmCode = fs.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim()
-    const asmResult = assemble(asmCode)
-    const dataCoe = dataSegToCoe(asmResult.dataSeg)
-    const textCoe = textSegToCoe(asmResult.textSeg)
-    fs.writeFileSync(path.join(outDir, 'dmem32.coe'), dataCoe)
-    fs.writeFileSync(path.join(outDir, 'prgmip32.coe'), textCoe)
-    fs.writeFileSync(path.join(outDir, 'serial.txt'), coeToTxt(textCoe, dataCoe))
-    stdoutPrint('[minisys-asm] Assembling done.')
+try {
+  if (args._.length === 0 || args._.length !== 2) {
+    stdoutPrint('Usage: node <some_path>/node.js <in_file> <out_dir> [-l]\n')
   } else {
-    // 进行链接
-    const biosContent = fs.readFileSync(path.join(__dirname, '../snippet/minisys-bios.asm')).toString()
-    const userAppASM = fs.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim()
-    const intEntryContent = fs.readFileSync(path.join(__dirname, '../snippet/minisys-interrupt-entry.asm')).toString()
-    const intHandlerContent = fs
-      .readFileSync(path.join(__dirname, '../snippet/minisys-interrupt-handler.asm'))
-      .toString()
-    // 把链接后的代码段进行汇编
-    let asm = (userAppASM + '\n')
-      .replace(/\r\n/g, '\n')
-      .replace(/#(.*)\n/g, '\n')
-      .split('\n')
-    // 挑出代码段和数据段
-    const dataSegStartLine = asm.findIndex(v => v.match(/\.data/))
-    const textSegStartLine = asm.findIndex(v => v.match(/\.text/))
-    assert(dataSegStartLine !== -1, '未找到数据段开始')
-    assert(textSegStartLine !== -1, '未找到代码段开始')
-    assert(dataSegStartLine < textSegStartLine, '数据段不能位于代码段之后')
-    // 链接完成后汇编
-    const allProgram =
-      asm.slice(dataSegStartLine, textSegStartLine).join('\n') +
-      '\n' +
-      '.text\n' +
-      linkAll(biosContent, asm.slice(textSegStartLine + 1).join('\n'), intEntryContent, intHandlerContent)
-    const all = assemble(allProgram)
-    const textCoe = textSegToCoe(all.textSeg)
-    const dataCoe = dataSegToCoe(all.dataSeg)
-    // 输出
-    fs.writeFileSync(path.join(outDir, 'prgmip32.coe'), textCoe)
-    fs.writeFileSync(path.join(outDir, 'dmem32.coe'), dataCoe)
-    fs.writeFileSync(path.join(outDir, 'serial.txt'), coeToTxt(textCoe, dataCoe))
-    fs.writeFileSync(path.join(outDir, 'linked.asm'), allProgram)
-    stdoutPrint(`[minisys-asm] Assembling done with linking. jOffset = ${jOffset} B.`)
+    const inFile = args._[0]
+    const outDir = args._[1]
+    const link = !!args.l
+    // @ts-ignore
+    globalThis._minisys = {
+      _userAppOffset: link ? jOffset : 0,
+    }
+    if (!link) {
+      const asmCode = fs.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim()
+      const asmResult = assemble(asmCode)
+      const dataCoe = dataSegToCoe(asmResult.dataSeg)
+      const textCoe = textSegToCoe(asmResult.textSeg)
+      fs.writeFileSync(path.join(outDir, 'dmem32.coe'), dataCoe)
+      fs.writeFileSync(path.join(outDir, 'prgmip32.coe'), textCoe)
+      fs.writeFileSync(path.join(outDir, 'serial.txt'), coeToTxt(textCoe, dataCoe))
+      stdoutPrint('[minisys-asm] Assembling done.')
+    } else {
+      // 进行链接
+      const biosContent = fs.readFileSync(path.join(__dirname, '../snippet/minisys-bios.asm')).toString()
+      const userAppASM = fs.readFileSync(inFile).toString('utf-8').replace(/\r\n/g, '\n').trim()
+      const intEntryContent = fs.readFileSync(path.join(__dirname, '../snippet/minisys-interrupt-entry.asm')).toString()
+      const intHandlerContent = fs
+        .readFileSync(path.join(__dirname, '../snippet/minisys-interrupt-handler.asm'))
+        .toString()
+      // 把链接后的代码段进行汇编
+      let asm = (userAppASM + '\n')
+        .replace(/\r\n/g, '\n')
+        .replace(/#(.*)\n/g, '\n')
+        .split('\n')
+      // 挑出代码段和数据段
+      const dataSegStartLine = asm.findIndex(v => v.match(/\.data/))
+      const textSegStartLine = asm.findIndex(v => v.match(/\.text/))
+      assert(dataSegStartLine !== -1, '未找到数据段开始')
+      assert(textSegStartLine !== -1, '未找到代码段开始')
+      assert(dataSegStartLine < textSegStartLine, '数据段不能位于代码段之后')
+      // 链接完成后汇编
+      const allProgram =
+        asm.slice(dataSegStartLine, textSegStartLine).join('\n') +
+        '\n' +
+        '.text\n' +
+        linkAll(biosContent, asm.slice(textSegStartLine + 1).join('\n'), intEntryContent, intHandlerContent)
+      const all = assemble(allProgram)
+      const textCoe = textSegToCoe(all.textSeg)
+      const dataCoe = dataSegToCoe(all.dataSeg)
+      // 输出
+      fs.writeFileSync(path.join(outDir, 'prgmip32.coe'), textCoe)
+      fs.writeFileSync(path.join(outDir, 'dmem32.coe'), dataCoe)
+      fs.writeFileSync(path.join(outDir, 'serial.txt'), coeToTxt(textCoe, dataCoe))
+      fs.writeFileSync(path.join(outDir, 'linked.asm'), allProgram)
+      stdoutPrint(`[minisys-asm] Assembling done with linking. jOffset = ${jOffset} B.`)
+    }
+  }
+} catch (ex) {
+  if (ex instanceof SeuError) {
+    stdoutPrint(`[SeuError] ${ex.message}\n`)
+  } else {
+    throw ex
   }
 }
